0635200a01772e59a0f78627c925f7f0c9eddc68
bojko108 <bojko108@gmail.com>
2019-04-05 22:50:57 +0300

    include dist

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,13 @@
-import git from 'git-rev-sync'
-import replace from 'rollup-plugin-replace'
-import babel from 'rollup-plugin-babel'
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import uglify from 'rollup-plugin-uglify'
+import git from 'git-rev-sync';
+import replace from 'rollup-plugin-replace';
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import uglify from 'rollup-plugin-uglify';
 
-var pjson = require('./package.json')
+var pjson = require('./package.json');
 
-const banner = `// topolis. See https://github.com/bjornharrtell/topolis
-// Licenses:
-// https://github.com/bjornharrtell/topolis/blob/master/LICENSE.txt
-`
+const banner = `// topolis. See https://github.com/bojko108/topolis`;
 
 export default {
   entry: 'src/topolis.js',
@@ -25,15 +22,18 @@ export default {
     resolve(),
     commonjs(),
     babel({
-      presets: [['env', {
-        modules: false,
-        targets: {
-          browsers: ['> 2%']
-        }
-      }]],
-      plugins: [
-        'external-helpers'
+      presets: [
+        [
+          'env',
+          {
+            modules: false,
+            targets: {
+              browsers: ['> 2%']
+            }
+          }
+        ]
       ],
+      plugins: ['external-helpers'],
       babelrc: false
     }),
     uglify({
@@ -42,4 +42,4 @@ export default {
       }
     })
   ]
-}
+};
